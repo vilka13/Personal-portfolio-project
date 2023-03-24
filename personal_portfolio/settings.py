e0388c5d83f7fc67a0faafa36a90f7caa1c68875
Stanislau Olshevski <lukumer1997@gmail.com>
2023-03-24 14:26:50 +0100

    part1

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!#!%y4@jaa^k8m!kd3hdf1my9cq=!h5w3h!_*#(!71dgw%^hqn1111'
+SECRET_KEY = 'django-insecure-!#!%y4@jaa^k8m!kd3hdf1my9cq=!h5w3h!_*#(!71dgw%^hqn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
